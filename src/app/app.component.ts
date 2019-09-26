36acf87ec7fc49cfd6e7f5106b9fa1ef86bb98b9
Eduardo Antonio Fernandez Diaz <fernandez9000@gmail.com>
2019-09-26 18:24:11 -0500

    Canvas added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { environment } from './../environments/environment';
-import { Component } from '@angular/core';
+import { Component, ViewChild, Input } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 
@@ -10,9 +10,14 @@ import { HttpHeaders } from '@angular/common/http';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  pictureUrl = 'https://upload.wikimedia.org/wikipedia/commons/c/c4/The_Beatles_in_America.JPG';
+  pictureUrl = 'http://sparethekids.com/wp-content/uploads/2017/03/cmgmh-14320822.png';
   title = 'FaceRecognition';
   response = '';
+
+  @Input() public width = 495;
+  @Input() public height = 445;
+  cx: CanvasRenderingContext2D;
+  @ViewChild('canvas', {static: true}) canvas;
 
   constructor(private http: HttpClient) {
   }
@@ -32,8 +37,25 @@ export class AppComponent {
       'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise');
 
     this.http.post(environment.apiEndpoint, { url: this.pictureUrl, params: httpParams } , { headers }).subscribe(
-      response => { this.response = JSON.stringify(response, null, 4); }
+      response => { this.response = JSON.stringify(response, null, 4); console.log(response[0]); this.drawCanvas(); }
       , error => { alert(error); }
     );
+  }
+
+  drawCanvas() {
+    const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
+    this.cx = canvasEl.getContext('2d');
+    const image = new Image();
+    image.src = this.pictureUrl;
+
+    canvasEl.width = image.width;
+    canvasEl.height = image.height;
+
+    this.cx.lineWidth = 3;
+    this.cx.lineCap = 'round';
+    this.cx.strokeStyle = '#000';
+    image.onload = () => {
+        this.cx.drawImage(image, 0, 0, image.width, image.height);
+    };
   }
 }
