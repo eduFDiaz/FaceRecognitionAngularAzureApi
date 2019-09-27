a54fc3612547d8be1516e7df136dbb07fe875db2
Eduardo Antonio Fernandez Diaz <fernandez9000@gmail.com>
2019-09-26 23:11:11 -0500

    canvas now is painted after image loads

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { environment } from './../environments/environment';
-import { Component, ViewChild, Input } from '@angular/core';
+import { Component, ViewChild, Input, OnInit } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 
@@ -9,49 +9,51 @@ import { HttpHeaders } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   pictureUrl = 'http://sparethekids.com/wp-content/uploads/2017/03/cmgmh-14320822.png';
   title = 'FaceRecognition';
   response = '';
   faces: any;
-
-  @Input() public width = 495;
-  @Input() public height = 445;
   cx: CanvasRenderingContext2D;
-  @ViewChild('canvas', {static: true}) canvas;
+  image = new Image();
+  @ViewChild('canvas', { static: true }) canvas;
 
   constructor(private http: HttpClient) {
   }
 
+  ngOnInit(): void {
+    this.image.src = this.pictureUrl;
+    if (this.image.complete) {
+      this.drawImage(this.image);
+    }
+  }
+
   detectFaces() {
     const headers = new HttpHeaders({
-      'Content-Type' : 'application/json',
+      'Content-Type': 'application/json',
       'Ocp-Apim-Subscription-Key': environment.key
     });
 
-    const options = {headers};
-
-    const httpParams =  new HttpParams({});
+    const httpParams = new HttpParams({});
     httpParams.append('returnFaceId', 'true');
     httpParams.append('returnFaceLandmarks', 'true');
     httpParams.append('returnFaceAttributes',
       'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise');
 
-    this.http.post(environment.apiEndpoint, { url: this.pictureUrl, params: httpParams } , { headers }).subscribe(
+    this.http.post(environment.apiEndpoint, { url: this.pictureUrl, params: httpParams }, { headers }).subscribe(
       response => {
         this.response = JSON.stringify(response, null, 4);
         this.faces = response;
-        this.drawCanvas();
+        this.drawImage(this.image);
+        this.drawFaces();
       }
       , error => { alert(error); }
     );
   }
 
-  drawCanvas() {
+  drawImage(image: any) {
     const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
     this.cx = canvasEl.getContext('2d');
-    const image = new Image();
-    image.src = this.pictureUrl;
 
     canvasEl.width = image.width;
     canvasEl.height = image.height;
@@ -59,16 +61,19 @@ export class AppComponent {
     this.cx.lineWidth = 3;
     this.cx.lineCap = 'round';
     this.cx.strokeStyle = '#000';
-    image.onload = () => {
-        this.cx.drawImage(image, 0, 0, image.width, image.height);
-        this.faces.forEach(face => {
-          const box = face.faceRectangle;
-          this.cx.beginPath();
-          this.cx.rect(box.left, box.top, box.width, box.height);
-          this.cx.lineWidth = 7;
-          this.cx.strokeStyle = 'black';
-          this.cx.stroke();
-        });
-    };
+    this.cx.drawImage(image, 0, 0, image.width, image.height);
+  }
+
+  drawFaces() {
+    this.drawImage(this.image);
+
+    this.faces.forEach(face => {
+      const box = face.faceRectangle;
+      this.cx.beginPath();
+      this.cx.rect(box.left, box.top, box.width, box.height);
+      this.cx.lineWidth = 7;
+      this.cx.strokeStyle = 'blue';
+      this.cx.stroke();
+    });
   }
 }
